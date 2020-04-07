3f1fb7a5ed9a6215e6856e0eb68f64e658a9c2fa
yasser <spookyframework@gmail.com>
2020-04-08 00:16:58 +0200

    feat(create-express-app): Add index.js file that require create-express-app internal file

---
+++ packages/create-express-app/index.js
@@ -0,0 +1,10 @@
+/*
+ * This file is part of the create-express-app package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require('./createExpressApp');
