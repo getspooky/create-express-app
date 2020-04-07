ba187f3a4e37288750874d57b4c64fb59eaac9e5
yasser <spookyframework@gmail.com>
2020-04-07 13:08:48 +0200

    test(create-express-app): Test if checkNodeVersion is passed

---
+++ packages/create-express-app/__tests__/index.test.js
@@ -0,0 +1,24 @@
+/*
+ * This file is part of the create-express-app package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+const createExpressApp = require('../createExpressApp');
+
+describe('Testing create-express-app package', function () {
+  it('should display message if the version is compatible', function () {
+    return createExpressApp.checkNodeVersion('8.1.0').then((response) => {
+      expect(response).toEqual('Node version compatible');
+    });
+  });
+
+  it('should throw error if the version is not compatible', function () {
+    return createExpressApp.checkNodeVersion('12.8.0').catch((error) => {
+      expect(error).toBeTruthy();
+    });
+  });
+});
