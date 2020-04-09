7d08e62f13c1bb47590576a7426a40e4e2b8229f
yasser <spookyframework@gmail.com>
2020-04-09 15:17:08 +0200

    setup(create-express-app): Add jest config file

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+/*
+ * This file is part of the create-express-app package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+'use strict';
+
+module.exports = {
+  roots: ['./'],
+  testRegex: '\\.(test|spec)\\.js$',
+  moduleFileExtensions: ['js', 'jsx'],
+  testPathIgnorePatterns: ['/(docsify|docs|node_modules)/'],
+  collectCoverage: true,
+  coverageReporters: ['json', 'lcov'],
+};
