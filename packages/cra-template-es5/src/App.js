26a25e03d193e3755f4e0ed0d0d02ac075329fb5
yasser <spookyframework@gmail.com>
2020-04-11 01:22:54 +0200

    feat(cra-template-es5): Use bodyParser middleware

--- packages/cra-template-es5/src/App.js
+++ packages/cra-template-es5/src/App.js
@@ -1,4 +1,5 @@
 var path = require('path'),
+  bodyParser = require('body-parser'),
   express = require('express');
 
 // Create global app object
@@ -6,6 +7,9 @@ const app = express();
 
 // Serve images, CSS files, HTML and JavaScript files in a directory named public.
 app.use('/static', express.static(path.join(__dirname, '../public')));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.redirect('/static/index.html');
