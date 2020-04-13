73782b11e3135df7939ae64b68571565a19287aa
yasser <spookyframework@gmail.com>
2020-04-13 13:19:44 +0200

    feat(jest-config-create-express-app): Add jest basic configuration for create-express-app

---
+++ packages/jest-config-create-express-app/jest.config.js
@@ -0,0 +1,21 @@
+/*
+ * This file is part of the create-express-app package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['src/**/*.{js,ts}'],
+  testRegex: '\\.(test|spec)\\.js$',
+  moduleFileExtensions: ['js', 'ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testPathIgnorePatterns: ['/(build|docs|node_modules)/'],
+  coverageReporters: ['json', 'lcov'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+};
