5d44a110a548419733424d5e01f804e7991c16f8
yasser <spookyframework@gmail.com>
2020-04-14 15:20:25 +0200

    refactor(express-scripts): Require expressArtisan file

---
+++ packages/express-artisan/index.js
@@ -0,0 +1,12 @@
+/*
+ * This file is part of the create-express-app package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+'use strict';
+
+require('./expressArtisan');
