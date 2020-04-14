f09f5f1ceb53d27d2b4731155ece985c40f5036d
yasser <spookyframework@gmail.com>
2020-04-14 23:52:33 +0200

    feat(express-artisan): Handle express-artisan start command

---
+++ packages/express-artisan/start.js
@@ -0,0 +1,56 @@
+/*
+ * This file is part of the express-artisan package.
+ *
+ * (c) Yasser Ameur El Idrissi <getspookydev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code. 
+ */
+
+'use strict';
+
+const path = require('path');
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+const ip = require('ip');
+const supportedProtocols = ['http', 'https'];
+// PORT environment variable
+const PORT = parseInt(process.env.PORT, 10) || 4200;
+// HOST environment variable
+const HOST = process.env.HOST || '127.0.0.1';
+
+var divider = chalk.gray('\n-----------------------------------');
+
+/**
+ * @internal
+ * @desc Create Express Server.
+ * @function
+ * @name start
+ * @returns {void}
+ */
+(function start() {
+  return inquirer
+    .prompt([{
+      type: 'list',
+      name: 'protocol',
+      message: 'Please specify the protocol!',
+      choices: [...supportedProtocols],
+    }, ])
+    .then(({
+        protocol
+      }) => require(protocol)
+      .createServer(require(
+        path.resolve(__dirname, process.cwd(), 'src', 'App')
+      ))
+      .listen(PORT, function (err) {
+        if (err) throw err;
+        console.log();
+        console.log('Server started ! ' + chalk.green('✓'));
+        console.log(`
+           ${chalk.bold('Access URLs:')}${divider}
+            Localhost: ${chalk.magenta(`http://${HOST}:${PORT}`)}
+            LAN: ${chalk.magenta(`http://${ip.address()}:${PORT}`)}
+       `);
+        console.log(chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`));
+      }));
+})();
