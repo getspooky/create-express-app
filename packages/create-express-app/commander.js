f24684bd3ede83e110dcaf80f6e86e04e7ff3dcd
yasser <spookyframework@gmail.com>
2020-04-19 14:54:51 +0200

    refactor(create-express-app): Add logs for version compatibility

--- packages/create-express-app/commander.js
+++ packages/create-express-app/commander.js
@@ -8,9 +8,9 @@
  */
 
 const chalk = require('chalk');
-const {
-  Command
-} = require('commander');
+const ora = require('ora');
+const { Command } = require('commander');
+const { _BANNER, _EMOJIS } = require('./interface');
 const {
   killProcess,
   checkNPMVersion,
@@ -19,10 +19,7 @@ const {
   initExpressApp,
 } = require('./createExpressApp');
 
-const {
-  version,
-  name
-} = require('./package.json');
+const { version, name, author } = require('./package.json');
 const program = new Command(name);
 program.version(version);
 
@@ -33,6 +30,19 @@ const versionCompatibility = {
   node: '8.1.0',
 };
 
+var spinner = {
+  check: ora('🎛 Checking Environment...'),
+  project: ora('🧬 Creating Project...'),
+};
+
+_BANNER('Create Express App');
+
+console.log(
+  `Created and maintained with ${chalk.bold.red(_EMOJIS.HEART)}  by ${author}`
+);
+console.log();
+console.log('Tools Version ' + chalk.green(version));
+
 program
   .command('init <projectName>')
   .alias('ci')
@@ -42,26 +52,38 @@ program
     'Setup the default folder structures to be used in the Project'
   )
   .action(function (projectName, action) {
+    spinner.check.start();
+    // Cheking NPM , Yarn and Node versions.
     Promise.all([
       checkNPMVersion(versionCompatibility.npm),
       checkYarnVersion(versionCompatibility.yarn),
       checkNodeVersion(versionCompatibility.node),
-      initExpressApp(projectName, action['directory']),
-    ]).catch((err) => {
-      console.log(chalk.red(err.message));
-      console.log(
-        'If you feel you have found a security issue or concern with create-express-app' +
-        '\n' +
-        'Please use the following link to create a new issue: '
-      );
-      console.log(
-        'Link : ' +
-        chalk.underline.blue(
-          'https://github.com/getspooky/create-express-app/issues'
-        )
-      );
-      killProcess();
-    });
+    ])
+      .then(([npm, yarn, node]) => {
+        spinner.check.stop();
+        console.log(npm.concat(chalk.green('✓')));
+        console.log(yarn.concat(chalk.green('✓')));
+        console.log(node.concat(chalk.green('✓')));
+        spinner.project.start();
+        initExpressApp(projectName, action.directory);
+      })
+      .then(() => spinner.project.stop())
+      .catch((err) => {
+        console.log();
+        console.log(chalk.red(err.message));
+        console.log(
+          'If you feel have found a security issue or concern with create-express-app' +
+            '\n' +
+            'Please use the following link to create a new issue: '
+        );
+        console.log(
+          'Link : ' +
+            chalk.underline.blue(
+              'https://github.com/getspooky/create-express-app/issues'
+            )
+        );
+        killProcess();
+      });
   });
 
 program.parse(process.argv);
