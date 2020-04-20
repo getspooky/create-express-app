54345636e6744a98a5efa0eb6e8ea35e2b8dad40
yasser <spookyframework@gmail.com>
2020-04-20 11:48:01 +0200

    feat(cra-template-es6): Integrate helmet

--- packages/cra-template-typescript/src/App.ts
+++ packages/cra-template-typescript/src/App.ts
@@ -1,9 +1,13 @@
 import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
+import helmet from 'helmet';
 
 // Create global app object
 const app = express();
+
+// Set various HTTP headers to help protect your app.
+app.use(helmet());
 
 // Serve images, CSS files, HTML and JavaScript files in a directory named public.
 app.use('/static', express.static(path.join(__dirname, '../public')));
