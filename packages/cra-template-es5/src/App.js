a41eea1491f1de007e9233aa27465ea59a8a89e7
yasser <spookyframework@gmail.com>
2020-04-29 00:08:21 +0200

    feat: Set the view engine to ejs

--- packages/cra-template-es5/src/App.js
+++ packages/cra-template-es5/src/App.js
@@ -9,6 +9,9 @@ const app = express();
 // Set various HTTP headers to help protect your app.
 app.use(helmet());
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 // Serve images, CSS files, HTML and JavaScript files in a directory named public.
 app.use('/static', express.static(path.join(__dirname, '../public')));
 
